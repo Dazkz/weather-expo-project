db57a77f18c65c6cdf30864eac8a9950603ab7eb
Dazkz <dazkza@gmail.com>
2021-04-04 15:39:28 +0300

    polished

--- src/components/WeatherInfo/WeatherInfo.js
+++ src/components/WeatherInfo/WeatherInfo.js
@@ -5,18 +5,21 @@ import React from "react";
 
 export function WeatherInfo({ data }) {
   if (!!data.main) {
-  return (
-    <View style={styles.container}>
-      <Text>{data.name}</Text>
-      <Text>{data.weather[0].main}</Text>
-      <Text>{data.main.temp}℃</Text>
-    </View>
-  );
+    return (
+      <View style={styles.container}>
+        <Text>{data.name}</Text>
+        <Text>{data.weather[0].main}</Text>
+        <Text>{data.main.temp}℃</Text>
+      </View>
+    );
   } else {
     return (
       <View style={styles.container}>
+        <Text>Что-то</Text>
+        <Text>Пошло</Text>
+        <Text>не так</Text>
       </View>
-    )
+    );
   }
 }
 
